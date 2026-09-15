d9db4a05dbe2cf7106d6281c6a212dd7205bc866
Jacred <118478922+Jacred@users.noreply.github.com>
2026-09-15 01:44:26 -0400

    Update DVs for various Pokémon and trainers

--- trainers/dvs.asm
+++ trainers/dvs.asm
@@ -92,117 +92,116 @@ GetTrainerDVs: ; 270c4
 	db $21, $d2 ; H
 
 .RedDVs
-	db $0E, $7A ; AA-j
-	db $CA, $7D ; AAAAAAAAAA
-	db $D0, $E4 ; OMASTAR
-	db $90, $A6 ; AATTVVV
-	db $03, $71 ; AIIIIIIRRR
-	db $AA, $64 ; aaabaaajss
+	db $FD, $DE ; PIKACHU
+	db $FD, $DE ; ESPEON
+	db $FD, $DE ; SNORLAX
+	db $FD, $DE ; VENUSAUR
+	db $FD, $DE ; CHARIZARD
+	db $FD, $DE ; BLASTOISE
 
 .AJDVs
-	db $89, $CF ; AAJRR  RRR
-	db $23, $80 ; KT
-	db $8A, $E4 ; AAAS RJ-1
-	db $F7, $6E ; AAAAAtttta
-	db $F8, $A0 ; BBBBBD
+	db $DC, $DD ; MEGANIUM
+	db $DC, $DD ; TYPHLOSION
+	db $DC, $DD ; FERALIGATR
+	db $DC, $DD ; UMBREON
+	db $DC, $DD ; SKARMORY
+	db $DC, $DD ; DONPHAN
 
 .AbeDVs
-	db $52, $7C ; AAbbABCabb
-	db $EC, $E5 ;  RRRRIIIIH
-	db $82, $EE ; IIII--??(U
-	db $0C, $9F ; AAA--××<MN><MN><MN>
-	db $2D, $F3 ; (ssjj <MN>..,
-	db $D8, $D5 ; RRQPO:<PK><PK><PK><PK>
+	db $FD, $DE ; ZAPDOS
+	db $FD, $DE ; NIDOKING
+	db $FD, $DE ; OMASTAR
+	db $FD, $DE ; VENOMOTH
+	db $FD, $DE ; LAPRAS
+	db $FD, $DE ; PIDGEOT
 
-.BabaDVs
-	db $94, $1D ; BEST
-	db $EF, $B0 ; DONG
-	db $46, $E8 ; EVER
-	db $EA, $AA ; DADA
-	db $4E, $2B ; ORGY
-	db $E8, $E5 ; DEKU
+.BabaDVs 
+	db $FD, $FF ; RAIKOU
+	db $CF, $FF ; ENTEI
+	db $AF, $FF ; SUICUNE
+	db $FF, $FF ; CELEBI
+	db $FF, $FF ; LUGIA
+	db $FF, $FF ; HO-OH
 
 TrainerClassDVs: ; 270d6
-	;  Atk  Spd
-	;  Def  Spc I tried to balance gyms to effective 69 total
-	db $FA, $F7 ; falkner
-	db $CC, $BA ; bugsy
-	db $7F, $DA ; whitney
-	db $CA, $FF ; morty
-	db $9E, $8F ; pryce
-	db $BF, $7A ; jasmine
-	db $FF, $97 ; chuck
-	db $D5, $CD ; clair
-	db $DD, $DD ; rival1
-	db $DD, $DD ; pokemon prof
+;	ATK DEF SPD SPC
+	db $9A, $77 ; falkner
+	db $88, $88 ; whitney
+	db $98, $88 ; bugsy
+	db $98, $88 ; morty
+	db $98, $88 ; pryce
+	db $98, $88 ; jasmine
+	db $98, $88 ; chuck
+	db $7C, $DD ; clair
+	db $DD, $DD ; silver1
+	db $98, $88 ; oak
 	db $DC, $DD ; will
-	db $DC, $DD ; cal
-	db $DD, $DC ; bruno
+	db $DC, $DD ; gold
+	db $DC, $DD ; bruno
 	db $7F, $DF ; karen
-	db $BD, $DD ; koga
-	db $DD, $DD ; champion
-	db $AF, $8F ; brock
-	db $F8, $CD ; misty
-	db $AC, $FF ; lt surge
-	db $98, $88 ; scientist
-	db $7D, $9F ; erika
+	db $DC, $DD ; koga
+	db $DC, $DD ; lance
+	db $98, $88 ; brock
+	db $78, $88 ; misty
+	db $98, $88 ; surge
+	db $98, $88 ; researcher
+	db $78, $88 ; erika
 	db $98, $88 ; youngster
-	db $98, $88 ; schoolboy
-	db $98, $88 ; bird keeper
+	db $98, $88 ; schoolkid
+	db $98, $88 ; birdkeeper
 	db $58, $88 ; lass
-	db $CA, $FF ; janine
-	db $D8, $C8 ; cooltrainerm
-	db $7C, $C8 ; cooltrainerf
+	db $98, $88 ; janine
+	db $D8, $C8 ; elite♂
+	db $7C, $C8 ; elite♀
 	db $69, $C8 ; beauty
 	db $98, $88 ; pokemaniac
-	db $D8, $A8 ; gruntm
+	db $D8, $A8 ; grunt♂
 	db $98, $88 ; gentleman
 	db $98, $88 ; skier
 	db $68, $88 ; teacher
-	db $78, $FF ; sabrina
+	db $7D, $87 ; sabrina
 	db $98, $88 ; bug catcher
 	db $98, $88 ; fisher
-	db $98, $88 ; swimmerm
-	db $78, $88 ; swimmerf
+	db $98, $88 ; swimmer♂
+	db $78, $88 ; swimmer♀
 	db $98, $88 ; sailor
 	db $98, $88 ; super nerd
-	db $98, $88 ; rival2
+	db $DD, $DD ; silver2
 	db $98, $88 ; guitarist
 	db $A8, $88 ; hiker
 	db $98, $88 ; biker
-	db $F8, $8F ; blaine
+	db $98, $88 ; blaine
 	db $98, $88 ; burglar
 	db $98, $88 ; firebreather
 	db $98, $88 ; juggler
 	db $98, $88 ; blackbelt
-	db $D8, $A8 ; executivem
-	db $98, $88 ; psychic
-	db $6A, $A8 ; picnicker
-	db $98, $88 ; camper
-	db $7E, $A8 ; executivef
+	db $D8, $A8 ; executive♂
+	db $98, $88 ; esper
+	db $6A, $A8 ; scout♀
+	db $98, $88 ; scout♂
+	db $7E, $A8 ; executive♀
 	db $98, $88 ; sage
-	db $78, $88 ; medium
+	db $78, $88 ; channeler
 	db $98, $88 ; boarder
-	db $98, $88 ; pokefanm
+	db $98, $88 ; pokefan♂
 	db $68, $8A ; kimono girl
 	db $68, $A8 ; twins
-	db $6D, $88 ; pokefanf
+	db $6D, $88 ; pokefan♀
 	db $FD, $DE ; red
-	db $DD, $DD ; blue
+	db $9D, $DD ; blue
 	db $98, $88 ; officer
-	db $7E, $A8 ; gruntf
-	db $aa, $aa ; mysticalman
-	db $88, $98 ; bill
-	db $DD, $DD ; elm
-	db $DD, $DD ; tppPc
-	db $D8, $C8 ; Giovanni
-	db $D8, $C8 ; coolsibs
-	db $DD, $DD ; rivalrb
-	db $DD, $DD ; rivalrb
-	db $98, $88 ; brockrb
-	db $78, $88 ; mistyrb
-	db $BA, $BA; baba
-	db $DD, $DD ; executiveegk
-	
+	db $7E, $A8 ; grunt♀
+	db $98, $88 ; eusine
+	db $98, $88 ; bill
+	db $FF, $FF ; elm
+	db $FF, $FF ; league's pc
+	db $D8, $A8 ; giovanni
+	db $DC, $C8 ; elites
+	db $EE, $FF ; rusty
+	db $EE, $FF ; azure
+	db $98, $88 ; brock
+	db $78, $88 ; misty
+	db $FF, $FF ; kris
+	db $D8, $A8 ; executive	
 ; 2715c
 
