bc1a21cb3d9f6473ac60c92f1c24a2bccd5ab1ed
Jacred <118478922+Jacred@users.noreply.github.com>
2026-09-16 08:55:32 -0400

    Update sfx_constants.asm

--- constants/sfx_constants.asm
+++ constants/sfx_constants.asm
@@ -1,209 +1,209 @@
 	const_def
-	const SFX_DEX_FANFARE_50_79           ; $00
-	const SFX_ITEM                        ; $01
-	const SFX_CAUGHT_MON                  ; $02
-	const SFX_POKEBALLS_PLACED_ON_TABLE   ; $03
-	const SFX_POTION                      ; $04
-	const SFX_FULL_HEAL                   ; $05
-	const SFX_MENU                        ; $06
-	const SFX_READ_TEXT                   ; $07
-	const SFX_READ_TEXT_2                 ; $08
-	const SFX_DEX_FANFARE_20_49           ; $09
-	const SFX_DEX_FANFARE_80_109          ; $0a
-	const SFX_POISON                      ; $0b
-	const SFX_GOT_SAFARI_BALLS            ; $0c
-	const SFX_BOOT_PC                     ; $0d
-	const SFX_SHUT_DOWN_PC                ; $0e
-	const SFX_CHOOSE_PC_OPTION            ; $0f
-	const SFX_ESCAPE_ROPE                 ; $10
-	const SFX_PUSH_BUTTON                 ; $11
-	const SFX_SECOND_PART_OF_ITEMFINDER   ; $12
-	const SFX_WARP_TO                     ; $13
-	const SFX_WARP_FROM                   ; $14
-	const SFX_CHANGE_DEX_MODE             ; $15
-	const SFX_JUMP_OVER_LEDGE             ; $16
-	const SFX_GRASS_RUSTLE                ; $17
-	const SFX_FLY                         ; $18
-	const SFX_WRONG                       ; $19
-	const SFX_SQUEAK                      ; $1a
-	const SFX_STRENGTH                    ; $1b
-	const SFX_BOAT                        ; $1c
-	const SFX_WALL_OPEN                   ; $1d
-	const SFX_PLACE_PUZZLE_PIECE_DOWN     ; $1e
-	const SFX_ENTER_DOOR                  ; $1f
-	const SFX_SWITCH_POKEMON              ; $20
-	const SFX_TALLY                       ; $21
-	const SFX_TRANSACTION                 ; $22
-	const SFX_EXIT_BUILDING               ; $23
-	const SFX_BUMP                        ; $24
-	const SFX_SAVE                        ; $25
-	const SFX_POKEFLUTE                   ; $26
-	const SFX_ELEVATOR_END                ; $27
-	const SFX_THROW_BALL                  ; $28
-	const SFX_BALL_POOF                   ; $29
-	const SFX_FAINT                       ; $2a
-	const SFX_RUN                         ; $2b
-	const SFX_SLOT_MACHINE_START          ; $2c
-	const SFX_FANFARE                     ; $2d
-	const SFX_PECK                        ; $2e
-	const SFX_KINESIS                     ; $2f
-	const SFX_LICK                        ; $30
-	const SFX_POUND                       ; $31
-	const SFX_MOVE_PUZZLE_PIECE           ; $32
-	const SFX_COMET_PUNCH                 ; $33
-	const SFX_MEGA_PUNCH                  ; $34
-	const SFX_SCRATCH                     ; $35
-	const SFX_VICEGRIP                    ; $36
-	const SFX_RAZOR_WIND                  ; $37
-	const SFX_CUT                         ; $38
-	const SFX_WING_ATTACK                 ; $39
-	const SFX_WHIRLWIND                   ; $3a
-	const SFX_BIND                        ; $3b
-	const SFX_VINE_WHIP                   ; $3c
-	const SFX_DOUBLE_KICK                 ; $3d
-	const SFX_MEGA_KICK                   ; $3e
-	const SFX_HEADBUTT                    ; $3f
-	const SFX_HORN_ATTACK                 ; $40
-	const SFX_TACKLE                      ; $41
-	const SFX_POISON_STING                ; $42
-	const SFX_POWDER                      ; $43
-	const SFX_DOUBLESLAP                  ; $44
-	const SFX_BITE                        ; $45
-	const SFX_JUMP_KICK                   ; $46
-	const SFX_STOMP                       ; $47
-	const SFX_TAIL_WHIP                   ; $48
-	const SFX_KARATE_CHOP                 ; $49
-	const SFX_SUBMISSION                  ; $4a
-	const SFX_WATER_GUN                   ; $4b
-	const SFX_SWORDS_DANCE                ; $4c
-	const SFX_THUNDER                     ; $4d
-	const SFX_SUPERSONIC                  ; $4e
-	const SFX_LEER                        ; $4f
-	const SFX_EMBER                       ; $50
-	const SFX_BUBBLEBEAM                  ; $51
-	const SFX_HYDRO_PUMP                  ; $52
-	const SFX_SURF                        ; $53
-	const SFX_PSYBEAM                     ; $54
-	const SFX_CHARGE                      ; $55
-	const SFX_THUNDERSHOCK                ; $56
-	const SFX_PSYCHIC                     ; $57
-	const SFX_SCREECH                     ; $58
-	const SFX_BONE_CLUB                   ; $59
-	const SFX_SHARPEN                     ; $5a
-	const SFX_EGG_BOMB                    ; $5b
-	const SFX_SING                        ; $5c
-	const SFX_HYPER_BEAM                  ; $5d
-	const SFX_SHINE                       ; $5e
-	const SFX_UNKNOWN_5F                  ; $5f
-	const SFX_UNKNOWN_60                  ; $60
-	const SFX_UNKNOWN_61                  ; $61
-	const SFX_UNKNOWN_62                  ; $62
-	const SFX_UNKNOWN_63                  ; $63
-	const SFX_BURN                        ; $64
-	const SFX_TITLE_SCREEN_ENTRANCE       ; $65
-	const SFX_UNKNOWN_66                  ; $66
-	const SFX_GET_COIN_FROM_SLOTS         ; $67
-	const SFX_PAY_DAY                     ; $68
-	const SFX_METRONOME                   ; $69
-	const SFX_CALL                        ; $6a
-	const SFX_HANG_UP                     ; $6b
-	const SFX_NO_SIGNAL                   ; $6c
-	const SFX_SANDSTORM                   ; $6d
-	const SFX_ELEVATOR                    ; $6e
-	const SFX_PROTECT                     ; $6f
-	const SFX_SKETCH                      ; $70
-	const SFX_RAIN_DANCE                  ; $71
-	const SFX_AEROBLAST                   ; $72
-	const SFX_SPARK                       ; $73
-	const SFX_CURSE                       ; $74
-	const SFX_RAGE                        ; $75
-	const SFX_THIEF                       ; $76
-	const SFX_THIEF_2                     ; $77
-	const SFX_SPIDER_WEB                  ; $78
-	const SFX_MIND_READER                 ; $79
-	const SFX_NIGHTMARE                   ; $7a
-	const SFX_SNORE                       ; $7b
-	const SFX_SWEET_KISS                  ; $7c
-	const SFX_SWEET_KISS_2                ; $7d
-	const SFX_BELLY_DRUM                  ; $7e
-	const SFX_UNKNOWN_7F                  ; $7f
-	const SFX_SLUDGE_BOMB                 ; $80
-	const SFX_FORESIGHT                   ; $81
-	const SFX_SPITE                       ; $82
-	const SFX_OUTRAGE                     ; $83
-	const SFX_PERISH_SONG                 ; $84
-	const SFX_GIGA_DRAIN                  ; $85
-	const SFX_ATTRACT                     ; $86
-	const SFX_KINESIS_2                   ; $87
-	const SFX_ZAP_CANNON                  ; $88
-	const SFX_MEAN_LOOK                   ; $89
-	const SFX_HEAL_BELL                   ; $8a
-	const SFX_RETURN                      ; $8b
-	const SFX_EXP_BAR                     ; $8c
-	const SFX_MILK_DRINK                  ; $8d
-	const SFX_PRESENT                     ; $8e
-	const SFX_MORNING_SUN                 ; $8f
-	const SFX_LEVEL_UP                    ; $90
-	const SFX_KEY_ITEM                    ; $91
-	const SFX_FANFARE_2                   ; $92
-	const SFX_REGISTER_PHONE_NUMBER       ; $93
-	const SFX_3RD_PLACE                   ; $94
-	const SFX_GET_EGG_FROM_DAYCARE_MAN    ; $95
-	const SFX_GET_EGG_FROM_DAYCARE_LADY   ; $96
-	const SFX_MOVE_DELETED                ; $97
-	const SFX_2ND_PLACE                   ; $98
-	const SFX_1ST_PLACE                   ; $99
-	const SFX_CHOOSE_A_CARD               ; $9a
-	const SFX_GET_TM                      ; $9b
-	const SFX_GET_BADGE                   ; $9c
-	const SFX_QUIT_SLOTS                  ; $9d
-	const SFX_EGG_CRACK                   ; $9e
-	const SFX_DEX_FANFARE_LESS_THAN_20    ; $9f
-	const SFX_DEX_FANFARE_140_169         ; $a0
-	const SFX_DEX_FANFARE_170_199         ; $a1
-	const SFX_DEX_FANFARE_200_229         ; $a2
-	const SFX_DEX_FANFARE_230_PLUS        ; $a3
-	const SFX_EVOLVED                     ; $a4
-	const SFX_MASTER_BALL                 ; $a5
-	const SFX_EGG_HATCH                   ; $a6
-	const SFX_GS_INTRO_CHARIZARD_FIREBALL ; $a7
-	const SFX_GS_INTRO_POKEMON_APPEARS    ; $a8
-	const SFX_FLASH                       ; $a9
-	const SFX_GAME_FREAK_LOGO_GS          ; $aa
-	const SFX_NOT_VERY_EFFECTIVE          ; $ab
-	const SFX_DAMAGE                      ; $ac
-	const SFX_SUPER_EFFECTIVE             ; $ad
-	const SFX_BALL_BOUNCE                 ; $ae
-	const SFX_MOONLIGHT                   ; $af
-	const SFX_ENCORE                      ; $b0
-	const SFX_BEAT_UP                     ; $b1
-	const SFX_BATON_PASS                  ; $b2
-	const SFX_BALL_WIGGLE                 ; $b3
-	const SFX_SWEET_SCENT                 ; $b4
-	const SFX_SWEET_SCENT_2               ; $b5
-	const SFX_HIT_END_OF_EXP_BAR          ; $b6
-	const SFX_GIVE_TRADEMON               ; $b7
-	const SFX_GET_TRADEMON                ; $b8
-	const SFX_TRAIN_ARRIVED               ; $b9
-	const SFX_STOP_SLOT                   ; $ba
-	const SFX_2_BOOPS                     ; $bb
-	const SFX_GLASS_TING                  ; $bc
-	const SFX_GLASS_TING_2                ; $bd
-	const SFX_INTRO_UNOWN_1               ; $be
-	const SFX_INTRO_UNOWN_2               ; $bf
-	const SFX_INTRO_UNOWN_3               ; $c0
-	const SFX_DITTO_POP_UP                ; $c1
-	const SFX_DITTO_TRANSFORM             ; $c2
-	const SFX_INTRO_SUICUNE_1             ; $c3
-	const SFX_INTRO_PICHU                 ; $c4
-	const SFX_INTRO_SUICUNE_2             ; $c5
-	const SFX_INTRO_SUICUNE_3             ; $c6
-	const SFX_DITTO_BOUNCE                ; $c7
-	const SFX_INTRO_SUICUNE_4             ; $c8
-	const SFX_GAME_FREAK_PRESENTS         ; $c9
-	const SFX_TINGLE                      ; $ca
-	const SFX_UNKNOWN_CB                  ; $cb
-	const SFX_TWO_PC_BEEPS                ; $cc
-	const SFX_4_NOTE_DITTY                ; $cd
-	const SFX_TWINKLE                     ; $ce
+	const SFX_DEX_FANFARE_50_79           ; $00 0
+	const SFX_ITEM                        ; $01 1
+	const SFX_CAUGHT_MON                  ; $02 2
+	const SFX_POKEBALLS_PLACED_ON_TABLE   ; $03 3
+	const SFX_POTION                      ; $04 4
+	const SFX_FULL_HEAL                   ; $05 5
+	const SFX_MENU                        ; $06 6
+	const SFX_READ_TEXT                   ; $07 7
+	const SFX_READ_TEXT_2                 ; $08 8
+	const SFX_DEX_FANFARE_20_49           ; $09 9
+	const SFX_DEX_FANFARE_80_109          ; $0a 10
+	const SFX_POISON                      ; $0b 11
+	const SFX_GOT_SAFARI_BALLS            ; $0c 12
+	const SFX_BOOT_PC                     ; $0d 13
+	const SFX_SHUT_DOWN_PC                ; $0e 14
+	const SFX_CHOOSE_PC_OPTION            ; $0f 15
+	const SFX_ESCAPE_ROPE                 ; $10 16
+	const SFX_PUSH_BUTTON                 ; $11 17
+	const SFX_SECOND_PART_OF_ITEMFINDER   ; $12 18
+	const SFX_WARP_TO                     ; $13 19
+	const SFX_WARP_FROM                   ; $14 20
+	const SFX_CHANGE_DEX_MODE             ; $15 21
+	const SFX_JUMP_OVER_LEDGE             ; $16 22 
+	const SFX_GRASS_RUSTLE                ; $17 23
+	const SFX_FLY                         ; $18 24
+	const SFX_WRONG                       ; $19 25
+	const SFX_SQUEAK                      ; $1a 26
+	const SFX_STRENGTH                    ; $1b 27
+	const SFX_BOAT                        ; $1c 28
+	const SFX_WALL_OPEN                   ; $1d 29
+	const SFX_PLACE_PUZZLE_PIECE_DOWN     ; $1e 30
+	const SFX_ENTER_DOOR                  ; $1f 31
+	const SFX_SWITCH_POKEMON              ; $20 32
+	const SFX_TALLY                       ; $21 33
+	const SFX_TRANSACTION                 ; $22 34
+	const SFX_EXIT_BUILDING               ; $23 35
+	const SFX_BUMP                        ; $24 36
+	const SFX_SAVE                        ; $25 37
+	const SFX_POKEFLUTE                   ; $26 38
+	const SFX_ELEVATOR_END                ; $27 39
+	const SFX_THROW_BALL                  ; $28 40
+	const SFX_BALL_POOF                   ; $29 41
+	const SFX_FAINT                       ; $2a 42
+	const SFX_RUN                         ; $2b 43
+	const SFX_SLOT_MACHINE_START          ; $2c 44
+	const SFX_FANFARE                     ; $2d 45
+	const SFX_PECK                        ; $2e 46
+	const SFX_KINESIS                     ; $2f 47
+	const SFX_LICK                        ; $30 48
+	const SFX_POUND                       ; $31 49
+	const SFX_MOVE_PUZZLE_PIECE           ; $32 50
+	const SFX_COMET_PUNCH                 ; $33 51
+	const SFX_MEGA_PUNCH                  ; $34 52
+	const SFX_SCRATCH                     ; $35 53
+	const SFX_VICEGRIP                    ; $36 54
+	const SFX_RAZOR_WIND                  ; $37 55
+	const SFX_CUT                         ; $38 56
+	const SFX_WING_ATTACK                 ; $39 57
+	const SFX_WHIRLWIND                   ; $3a 58
+	const SFX_BIND                        ; $3b 59
+	const SFX_VINE_WHIP                   ; $3c 60
+	const SFX_DOUBLE_KICK                 ; $3d 61
+	const SFX_MEGA_KICK                   ; $3e 62
+	const SFX_HEADBUTT                    ; $3f 63
+	const SFX_HORN_ATTACK                 ; $40 64
+	const SFX_TACKLE                      ; $41 65
+	const SFX_POISON_STING                ; $42 66
+	const SFX_POWDER                      ; $43 67
+	const SFX_DOUBLESLAP                  ; $44 68
+	const SFX_BITE                        ; $45 69
+	const SFX_JUMP_KICK                   ; $46 70
+	const SFX_STOMP                       ; $47 71
+	const SFX_TAIL_WHIP                   ; $48 72
+	const SFX_KARATE_CHOP                 ; $49 73
+	const SFX_SUBMISSION                  ; $4a 74
+	const SFX_WATER_GUN                   ; $4b 75
+	const SFX_SWORDS_DANCE                ; $4c 76
+	const SFX_THUNDER                     ; $4d 77
+	const SFX_SUPERSONIC                  ; $4e 78
+	const SFX_LEER                        ; $4f 79
+	const SFX_EMBER                       ; $50 80
+	const SFX_BUBBLEBEAM                  ; $51 81
+	const SFX_HYDRO_PUMP                  ; $52 82
+	const SFX_SURF                        ; $53 83
+	const SFX_PSYBEAM                     ; $54 84
+	const SFX_CHARGE                      ; $55 85
+	const SFX_THUNDERSHOCK                ; $56 86
+	const SFX_PSYCHIC                     ; $57 87
+	const SFX_SCREECH                     ; $58 88
+	const SFX_BONE_CLUB                   ; $59 89
+	const SFX_SHARPEN                     ; $5a 90
+	const SFX_EGG_BOMB                    ; $5b 91
+	const SFX_SING                        ; $5c 92
+	const SFX_HYPER_BEAM                  ; $5d 93
+	const SFX_SHINE                       ; $5e 94
+	const SFX_UNKNOWN_5F                  ; $5f 95
+	const SFX_UNKNOWN_60                  ; $60 96
+	const SFX_UNKNOWN_61                  ; $61 97
+	const SFX_UNKNOWN_62                  ; $62 98
+	const SFX_UNKNOWN_63                  ; $63 99
+	const SFX_BURN                        ; $64 100
+	const SFX_TITLE_SCREEN_ENTRANCE       ; $65 101
+	const SFX_UNKNOWN_66                  ; $66 102
+	const SFX_GET_COIN_FROM_SLOTS         ; $67 103
+	const SFX_PAY_DAY                     ; $68 104
+	const SFX_METRONOME                   ; $69 105
+	const SFX_CALL                        ; $6a 106
+	const SFX_HANG_UP                     ; $6b 107
+	const SFX_NO_SIGNAL                   ; $6c 108
+	const SFX_SANDSTORM                   ; $6d 109
+	const SFX_ELEVATOR                    ; $6e 110
+	const SFX_PROTECT                     ; $6f 111
+	const SFX_SKETCH                      ; $70 112 
+	const SFX_RAIN_DANCE                  ; $71 113
+	const SFX_AEROBLAST                   ; $72 114
+	const SFX_SPARK                       ; $73 115
+	const SFX_CURSE                       ; $74 116
+	const SFX_RAGE                        ; $75 117
+	const SFX_THIEF                       ; $76 118
+	const SFX_THIEF_2                     ; $77 119
+	const SFX_SPIDER_WEB                  ; $78 120
+	const SFX_MIND_READER                 ; $79 121
+	const SFX_NIGHTMARE                   ; $7a 122
+	const SFX_SNORE                       ; $7b 123
+	const SFX_SWEET_KISS                  ; $7c 124
+	const SFX_SWEET_KISS_2                ; $7d 125
+	const SFX_BELLY_DRUM                  ; $7e 126
+	const SFX_UNKNOWN_7F                  ; $7f 127
+	const SFX_SLUDGE_BOMB                 ; $80 128
+	const SFX_FORESIGHT                   ; $81 129
+	const SFX_SPITE                       ; $82 130
+	const SFX_OUTRAGE                     ; $83 131
+	const SFX_PERISH_SONG                 ; $84 132
+	const SFX_GIGA_DRAIN                  ; $85 133
+	const SFX_ATTRACT                     ; $86 134
+	const SFX_KINESIS_2                   ; $87 135
+	const SFX_ZAP_CANNON                  ; $88 136
+	const SFX_MEAN_LOOK                   ; $89 137
+	const SFX_HEAL_BELL                   ; $8a 138
+	const SFX_RETURN                      ; $8b 139
+	const SFX_EXP_BAR                     ; $8c 140
+	const SFX_MILK_DRINK                  ; $8d 141
+	const SFX_PRESENT                     ; $8e 142
+	const SFX_MORNING_SUN                 ; $8f 143
+	const SFX_LEVEL_UP                    ; $90 144
+	const SFX_KEY_ITEM                    ; $91 145
+	const SFX_FANFARE_2                   ; $92 146
+	const SFX_REGISTER_PHONE_NUMBER       ; $93 147
+	const SFX_3RD_PLACE                   ; $94 148
+	const SFX_GET_EGG_FROM_DAYCARE_MAN    ; $95 149
+	const SFX_GET_EGG_FROM_DAYCARE_LADY   ; $96 150
+	const SFX_MOVE_DELETED                ; $97 151
+	const SFX_2ND_PLACE                   ; $98 152
+	const SFX_1ST_PLACE                   ; $99 153
+	const SFX_CHOOSE_A_CARD               ; $9a 154
+	const SFX_GET_TM                      ; $9b 155
+	const SFX_GET_BADGE                   ; $9c 156
+	const SFX_QUIT_SLOTS                  ; $9d 157
+	const SFX_EGG_CRACK                   ; $9e 158
+	const SFX_DEX_FANFARE_LESS_THAN_20    ; $9f 159
+	const SFX_DEX_FANFARE_140_169         ; $a0 160
+	const SFX_DEX_FANFARE_170_199         ; $a1 161
+	const SFX_DEX_FANFARE_200_229         ; $a2 162
+	const SFX_DEX_FANFARE_230_PLUS        ; $a3 163
+	const SFX_EVOLVED                     ; $a4 164
+	const SFX_MASTER_BALL                 ; $a5 165
+	const SFX_EGG_HATCH                   ; $a6 166
+	const SFX_GS_INTRO_CHARIZARD_FIREBALL ; $a7 167
+	const SFX_GS_INTRO_POKEMON_APPEARS    ; $a8 168
+	const SFX_FLASH                       ; $a9 169
+	const SFX_GAME_FREAK_LOGO_GS          ; $aa 170
+	const SFX_NOT_VERY_EFFECTIVE          ; $ab 171
+	const SFX_DAMAGE                      ; $ac 172
+	const SFX_SUPER_EFFECTIVE             ; $ad 173
+	const SFX_BALL_BOUNCE                 ; $ae 174
+	const SFX_MOONLIGHT                   ; $af 175
+	const SFX_ENCORE                      ; $b0 176
+	const SFX_BEAT_UP                     ; $b1 177
+	const SFX_BATON_PASS                  ; $b2 178
+	const SFX_BALL_WIGGLE                 ; $b3 179
+	const SFX_SWEET_SCENT                 ; $b4 180
+	const SFX_SWEET_SCENT_2               ; $b5 181
+	const SFX_HIT_END_OF_EXP_BAR          ; $b6 182
+	const SFX_GIVE_TRADEMON               ; $b7 183
+	const SFX_GET_TRADEMON                ; $b8 184
+	const SFX_TRAIN_ARRIVED               ; $b9 185
+	const SFX_STOP_SLOT                   ; $ba 186
+	const SFX_2_BOOPS                     ; $bb 187
+	const SFX_GLASS_TING                  ; $bc 188
+	const SFX_GLASS_TING_2                ; $bd 189
+	const SFX_INTRO_UNOWN_1               ; $be 190
+	const SFX_INTRO_UNOWN_2               ; $bf 191
+	const SFX_INTRO_UNOWN_3               ; $c0 192
+	const SFX_DITTO_POP_UP                ; $c1 193
+	const SFX_DITTO_TRANSFORM             ; $c2 194
+	const SFX_INTRO_SUICUNE_1             ; $c3 195
+	const SFX_INTRO_PICHU                 ; $c4 196
+	const SFX_INTRO_SUICUNE_2             ; $c5 197
+	const SFX_INTRO_SUICUNE_3             ; $c6 198
+	const SFX_DITTO_BOUNCE                ; $c7 199
+	const SFX_INTRO_SUICUNE_4             ; $c8 200
+	const SFX_GAME_FREAK_PRESENTS         ; $c9 201
+	const SFX_TINGLE                      ; $ca 202
+	const SFX_UNKNOWN_CB                  ; $cb 203
+	const SFX_TWO_PC_BEEPS                ; $cc 204
+	const SFX_4_NOTE_DITTY                ; $cd 205
+	const SFX_TWINKLE                     ; $ce 206
 NUM_SFX EQU const_value
