a768277c7de28e71cdb9b927f4b0db2de58e73eb
Jacred <118478922+Jacred@users.noreply.github.com>
2026-09-18 13:07:11 -0400

    Add files via upload

---
+++ data/pokedex/252.asm
@@ -0,0 +1,12 @@
+; PhanceroPokedexEntry: ; 0x1d18cc
+	db "MISSING@" ; species name
+	dw 000, 000 ; height, width
+
+	db   "This elusive bird"
+	next "uses illusions to"
+	next "conceal itself."
+	page "Can only be seen"
+	next "under unusual"
+	next "circumstances.@"
+; 0x1d1931
+
